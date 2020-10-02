d5bd2bacccb2e3d1adb9ab4dc5f0fbc9991230a1
acunil <locamos@gmail.com>
2020-10-02 13:34:45 +0100

    figuring exchange rate logic

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -8,10 +8,37 @@ export const renderDom = info => {
     currencyCode = info.restCountries.data.currencies[0].code,
     currencyName = info.restCountries.data.currencies[0].name,
     currencySymbol = info.restCountries.data.currencies[0].symbol,
-    exchangeRates = info.openExchangeRates.data.rates[currencyCode];
+    exchangeRates = info.openExchangeRates.data.rates,
+    currencyValueToUSD = exchangeRates[currencyCode];
 
   // Proceed to populate DOM with jQuery
   //
   //
   //
+  $("#country-name span").html(countryName);
+  $("#population span").html(population);
+  $("#languages span").html(languages); // switch to forEach for multiple langs
+  $("#capital-name span").html(capitalName);
+  $("#currency-name span").html(currencyName + " " + currencySymbol);
+  $("#currency-code span").html(currencyCode);
+
+  // exchange rates here, placeholders only!!!!
+  $("#GBP span").html(exchangeRates.GBP);
+  $("#USD span").html(exchangeRates.USD);
+  $("#EUR span").html(exchangeRates.EUR);
+};
+
+// exchangeRate calculator
+const calculateExchangeRates = () => {
+  let n = {
+    gbp: exchangeRates.GBP,
+    usd: exchangeRates.USD,
+    eur: exchangeRates.EUR,
+    self: exchangeRates[currencyCode],
+  };
+
+  for (let code in n) {
+    n[code] = Math.round(n[code] * 100) / 100;
+    n[code] /= n.self;
+  }
 };
